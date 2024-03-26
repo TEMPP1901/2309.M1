a17206abc8f03e523840e469794f8cb53d48a2cc
TEMPP1901 <temppol1901@gmail.com>
2024-03-26 09:54:26 +0700

    aaa

--- edunext/loptruutuong.php
+++ edunext/loptruutuong.php
@@ -1,6 +1,11 @@
 <?php
 abstract class Animal
 {
+    public $name;
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
     abstract public function makeSound();
 }
 
@@ -8,7 +13,7 @@ class Dog extends Animal
 {
     public function makeSound()
     {
-        echo "Woof!\n";
+        echo $this->name . ": Woof!\n";
     }
 }
 
@@ -16,13 +21,13 @@ class Cat extends Animal
 {
     public function makeSound()
     {
-        echo "Meow!\n";
+        echo $this->name . ": Meow!\n";
     }
 }
 
- //Lớp `Animal` là lớp trừu tượng, có phương thức trừu tượng `makeSound`.
- //Lớp `Dog` và `Cat` kế thừa từ lớp `Animal`, và định nghĩa lại phương thức `makeSound`.
-$spike=new Dog();
+//Lớp `Animal` là lớp trừu tượng, có phương thức trừu tượng `makeSound`.
+//Lớp `Dog` và `Cat` kế thừa từ lớp `Animal`, và định nghĩa lại phương thức `makeSound`.
+$spike = new Dog("Spike");
 $spike->makeSound();
-$misty=new Cat();
+$misty = new Cat("Misty");
 $misty->makeSound();
